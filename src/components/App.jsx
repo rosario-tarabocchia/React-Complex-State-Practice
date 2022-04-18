d81bf9424d23cc025b3cda5241ad4572382dc7de
Rosario Tarabocchia <84477640+rosario-tarabocchia@users.noreply.github.com>
2022-04-18 13:18:22 -0400

    Challenge Complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,16 +7,42 @@ function App() {
     email: ""
   });
 
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setContact((prevValue) => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: prevValue.lName,
+          email: prevValue.email
+        };
+      } else if (name === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: value,
+          email: prevValue.email
+        };
+      } else if (name === "email") {
+        return {
+          fName: prevValue.fName,
+          lName: prevValue.lName,
+          email: value
+        };
+      }
+    });
+  }
+
   return (
     <div className="container">
       <h1>
         Hello {contact.fName} {contact.lName}
       </h1>
       <p>{contact.email}</p>
-      <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <input name="email" placeholder="Email" />
+      <form onChange={handleChange}>
+        <input name="fName" placeholder="First Name" value={contact.fName} />
+        <input name="lName" placeholder="Last Name" value={contact.lName} />
+        <input name="email" placeholder="Email" value={contact.email} />
         <button>Submit</button>
       </form>
     </div>
